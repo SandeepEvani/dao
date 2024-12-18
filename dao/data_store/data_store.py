2d8537bdf7685e8cb42110afd2108fe33e3846a4
Evani Sandeep <evani.sandeep@ganitinc.com>
2024-12-18 21:05:39 +0530

    Add docstrings and comments

--- dao/data_store/data_store.py
+++ dao/data_store/data_store.py
@@ -3,25 +3,67 @@
 
 
 class DataStore:
+    """
+    The Data Store class is a virtual representation of the
+    Data Store present in the working environment to which we
+    read and write data from. A Data store will have a primary
+    interface class and optional secondary interface classes to
+    access the data stored within.
+    """
 
     __data_stores = {}
 
     def __init__(self, name, **properties):
+        """
+        Initializes the Data Store Object
+
+        :param name: Name of the Data Store
+        :param properties: Any external properties for a data store if required
+        """
+
         if name in self.__data_stores:
             raise Exception("Duplicate DataStore is detected")
+
         self.name = name
         self.__data_stores.update({name: self})
 
-    def set_primary_interface_class(self, class_):
+    def set_primary_interface_class(self, class_) -> None:
+        """
+        Registers the primary interface class with the data store object
+        The primary interface class/object is used when there are no
+        other specified parameters
+
+        :param class_: The class object which is used as the interface
+        :return: None
+        """
         self.interface_class = class_
 
     def set_primary_interface_object(self, obj):
+        """
+        Registers the primary interface object with the data store object
+        The primary interface class/object is used when there are no
+        other specified parameters
+
+        :param obj: The object which is used as the primary interface
+        :return: None
+        """
+
         self.interface_object = obj
 
     def get_interface_objects(self):
+        """
+        Returns a list of interface objects which are previously registered
+
+        :return: list: interface objects
+        """
         return [self.interface_object]
 
     def get_details(self):
+        """
+        Returns an generator which returns the name, class and the object
+
+        :return: A generator which gets the name, class and the object
+        """
         for interface_class, interface_object in [
             (self.interface_class, self.interface_object),
         ]:
--- dao/data_store/data_store_factory.py
+++ dao/data_store/data_store_factory.py
@@ -10,8 +10,14 @@ from .data_store import DataStore
 
 class DataStoreFactory:
 
+    """
+    Creates multiple instances of the DataStore class
+    at the runtime.
+    """
+
     def __init__(self, ds_conf_loc):
         """
+        Initializes the data store class
 
         :param ds_conf_loc: Path to the DataStore Config file
         """
@@ -22,22 +28,36 @@ class DataStoreFactory:
 
     def create_data_stores(self):
         """
+        Creates the data stores from the given datastore confs
 
-        :return:
+        :return: the identifier and the data store object
         """
 
+        # Gets the list of data stores from the confs
         data_stores = self.confs["data_stores"]
         for identifier, properties in data_stores.items():
+
+            # infer the class and location from the properties
             interface_class_name = properties["interface_class"]
             class_path = properties["interface_class_location"]
 
+            # imports the modules from the given location
             interface_module = import_module(class_path)
             interface_class = getattr(interface_module, interface_class_name)
-            interface_object = call(interface_class, **properties["default_configs"])
 
+            # Instantiate the interface class
+            if properties == {}:
+                interface_object = call(interface_class)
+            else:
+                interface_object = call(interface_class, **properties["default_configs"])
+
+            # Create the data store object
             data_store = DataStore(identifier)
 
+            # Set the interface class and objects
             data_store.set_primary_interface_class(interface_class)
             data_store.set_primary_interface_object(interface_object)
+
+            # TODO: add secondary interface functionality
 
             yield identifier, data_store
