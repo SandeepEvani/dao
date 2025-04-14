690500ba9c9ecd91263e9521dc8fc7b4aa747990
Sandeep Evani <evani.sandeep@ganitinc.com>
2025-04-14 16:31:21 +0530

    Run tests

--- examples/main.py
+++ examples/main.py
@@ -4,10 +4,12 @@ from dao import dao
 
 if __name__ == "__main__":
 
-    dao.init("examples/data_stores.json", lazy=False)
+    dao.init("examples/data_stores.json", lazy=True)
 
     table = catalog.get("raw.customer")
 
     result = dao.write(data="Hello", data_object=table, arg1=1)
+    result = dao.write(data="Hello", data_object=table, arg1=1, dao_interface_class="Hudi")
+    result = dao.write(data="Hello", data_object=table, arg1=1, dao_interface_class="Delta")
 
     print(result)
