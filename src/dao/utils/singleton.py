4f0194193e4dd0caab5fcd053bee0a336b109896
Sandeep Evani <evani.sandeep@ganitinc.com>
2025-09-28 19:12:03 +0530

    Add Utils to create a singleton class

---
+++ src/dao/utils/singleton.py
@@ -0,0 +1,26 @@
+# singleton.py
+# Contains helper functions to create singleton objects
+
+from functools import wraps
+
+
+def singleton(class_):
+    """A Class decorator used to create singleton classes"""
+
+    # Placeholder to store the class instance
+    object_instance = None
+
+    @wraps(class_)
+    def wrapper(*args, **kwargs):
+        """Default wrapper which creates and stores the class instance"""
+
+        # Refer to the object_instance in the parent function making it a closure
+        nonlocal object_instance
+
+        # Create instance if None
+        if object_instance is None:
+            object_instance = class_(*args, **kwargs)
+
+        return object_instance
+
+    return wrapper
