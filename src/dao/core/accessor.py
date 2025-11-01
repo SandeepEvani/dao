e0edef012711618c74777572c5fc4b4b0119acda
Sandeep Evani <evani.sandeep@ganitinc.com>
2025-11-02 01:32:31 +0530

    Add Docstrings

--- src/dao/core/accessor.py
+++ src/dao/core/accessor.py
@@ -11,12 +11,22 @@ from dao.data_store import DataStoreFactory, DataStoreRegistry
 
 
 class DataAccessor:
-    """ """
+    """Data descriptor that provides data access logic for DAO methods.
+
+    This class handles the routing and execution of data access operations
+    by coordinating with data stores, routers, and signature processors.
+    """
 
     _initialized = set()
 
     def __init__(self, signature: Optional[dict] = None, doc_string: Optional[str] = None):
-        """ """
+        """Initialize the DataAccessor with method signature and documentation.
+
+        Args:
+            signature: Optional custom signature for the accessor method.
+                      If None, uses the signature of the accessor_function.
+            doc_string: Optional documentation string for the accessor method.
+        """
         if signature is None:
             self.signature = signature_generator(self.accessor_function)
         self.doc_string = doc_string
@@ -25,37 +35,67 @@ class DataAccessor:
         self.data_store_factory = DataStoreFactory()
 
     def __get__(self, instance, owner):
-        """ """
+        """Retrieve the accessor function when accessed as a descriptor.
+
+        Args:
+            instance: The instance that the descriptor is accessed from.
+            owner: The class that owns the descriptor.
+
+        Returns:
+            The bound accessor function.
+        """
         return self.accessor_function
 
     def __set_name__(self, owner: type, name: str):
-        """Get the name and owner of the accessor method"""
+        """Set the name and owner when descriptor is assigned to a class.
+
+        Args:
+            owner: The class that owns this descriptor.
+            name: The name of the attribute in the owner class.
+        """
         self.owner = owner
         self.action = name
         self.router = Router(name)
 
-    def create_accessor_function(self): ...
+    def create_accessor_function(self):
+        """Create and configure the accessor function.
+
+        This method should be implemented to create the actual accessor
+        function that will handle data operations.
+        """
+        ...
 
     def accessor_function(self, data_object, **kwargs):
-        """
+        """Main accessor function that handles data operations.
 
-        :param data_object:
-        :param kwargs:
-        :return:
-        """
+        This function is called when the accessor is invoked and coordinates
+        the data access logic with the appropriate data store.
 
+        Args:
+            data_object: The data object containing data store information.
+            **kwargs: Additional arguments for the data operation.
+
+        Returns:
+            The result of the data access operation.
+        """
         # calls the data access logic
         result = self.data_access_logic(locals().copy())
 
         return result
 
     def data_access_logic(self, provided_args):
-        """The Data access logic used by the read and write methods in the DAO.
+        """Execute the core data access logic for DAO operations.
 
-        :param provided_args: The arg set provided to the DAO
-        :return
+        This method processes arguments, determines the appropriate data store,
+        initializes routes if needed, and executes the chosen method.
+
+        Args:
+            provided_args: Dictionary of arguments provided to the DAO method.
+
+        Returns:
+            The result of the executed data access method.
         """
-        method_args, conf_args = self._preprocess_args(provided_args)
+        method_args, conf_args = self.process_args(provided_args)
 
         argument_signature = self.signature_factory.create_argument_signature(method_args, self.signature)
 
@@ -82,13 +122,20 @@ class DataAccessor:
 
         return method(**method_args)
 
-    def _preprocess_args(self, provided_args):
-        """Filters, flattens (only the kwarg) and segregates the args.
+    def process_args(self, provided_args):
+        """process and segregate arguments for method execution.
 
-        :param provided_args: Args provided through the DAO
-        :return:
+        Filters out unwanted arguments, flattens keyword arguments, and
+        separates method arguments from configuration arguments.
+
+        Args:
+            provided_args: Dictionary of arguments provided to the DAO method.
+
+        Returns:
+            Tuple of (method_args, conf_args) where:
+                method_args: Arguments intended for the data method.
+                conf_args: Configuration arguments for the DAO system.
         """
-
         # Filters the args from unwanted variables
         self._filter_args(provided_args, self.signature)
 
@@ -108,11 +155,11 @@ class DataAccessor:
 
     @staticmethod
     def _filter_args(args, signature):
-        """Filters the local variable set to only the required args.
+        """Filter arguments to only include those defined in the method signature.
 
-        :param args: The local variables set
-        :param signature: The Signature of the respective method (read/write)
-        :return:
+        Args:
+            args: Dictionary of local variables/arguments.
+            signature: The signature object defining valid parameters.
         """
         keys = list(args.keys())
         for arg_name in keys:
@@ -121,13 +168,17 @@ class DataAccessor:
 
     @staticmethod
     def _segregate_args(args, segregation_prefix="dao_"):
-        """Segregates the conf args and method args.
+        """Segregate configuration arguments from method arguments.
 
-        :param args: set of provided args to the DAO
-        :param segregation_prefix: The prefix on which the argument segregation happens
-        :return: Tuple: method args and config args
+        Args:
+            args: Dictionary of arguments to segregate.
+            segregation_prefix: Prefix used to identify configuration arguments.
+
+        Returns:
+            Tuple of (method_args, conf_args) where:
+                method_args: Regular method arguments.
+                conf_args: Configuration arguments with the specified prefix.
         """
-
         confs = {}
         method_args = {}
 
--- src/dao/core/dao.py
+++ src/dao/core/dao.py
@@ -8,18 +8,106 @@ from dao.utils.singleton import singleton
 
 @singleton
 class DataAccessObject:
-    def __init__(self, data_stores: dict):
-        """Data Access Object is a convenience class, acting as an interface to access data,
-        data stores and perform many other data operations, All this heavy lifting is
-        abstracted from the user using the package and gives an easy and clean interface while writing
-        the main application code
-        """
+    """Data Access Object providing a unified interface for data operations.
 
+    A singleton class that serves as the main entry point for all data access
+    operations, abstracting the complexity of underlying data stores and
+    providing a clean, consistent API for application code.
+    """
+
+    def __init__(self, data_stores: dict):
+        """Initialize the Data Access Object with configured data stores.
+
+        Args:
+            data_stores: Dictionary of data store configurations where keys are
+                        data store names and values are their configurations.
+        """
         # Register the data stores to the registry for common access
         self.data_stores = data_stores
         DataStoreRegistry.initialize(data_stores)
 
     # Create data accessor methods to access the respective data methods
-    read = DataAccessor()
-    write = DataAccessor()
-    run = DataAccessor()
+    # DataAccessor descriptor helps you create multiple data access actions across
+
+    read = DataAccessor(
+        doc_string="""Read data from data stores.
+
+    Provides read operations for retrieving data from configured data stores.
+    """
+    )
+
+    write = DataAccessor(
+        doc_string="""Write data to data stores.
+
+    Provides write operations for storing data to configured data stores.
+    """
+    )
+
+    upsert = DataAccessor(
+        doc_string="""Upsert (update or insert) data in data stores.
+
+    Provides upsert operations that update existing records or insert new ones
+    if they don't exist.
+    """
+    )
+
+    delete = DataAccessor(
+        doc_string="""Delete data from data stores.
+
+    Provides delete operations for removing data from configured data stores.
+    """
+    )
+
+    run = DataAccessor(
+        doc_string="""Execute data operations or commands.
+
+    Provides operations for executing custom commands or procedures on data stores.
+    """
+    )
+
+    list = DataAccessor(
+        doc_string="""List data elements or collections.
+
+    Provides operations for listing available data elements, collections,
+    or resources in data stores.
+    """
+    )
+
+    count = DataAccessor(
+        doc_string="""Count data elements.
+
+    Provides operations for counting data elements that match specific criteria.
+    """
+    )
+
+    move = DataAccessor(
+        doc_string="""Move data between locations or stores.
+
+    Provides operations for moving data from one location to another within
+    or between data stores.
+    """
+    )
+
+    copy = DataAccessor(
+        doc_string="""Copy data between locations or stores.
+
+    Provides operations for copying data from one location to another within
+    or between data stores.
+    """
+    )
+
+    read_stream = DataAccessor(
+        doc_string="""Read data as a stream.
+
+    Provides streaming read operations for handling large datasets or
+    continuous data flows.
+    """
+    )
+
+    write_stream = DataAccessor(
+        doc_string="""Write data as a stream.
+
+    Provides streaming write operations for handling large datasets or
+    continuous data flows.
+    """
+    )
