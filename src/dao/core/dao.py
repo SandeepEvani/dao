3dc03a92f9a3804d98bafb30be9399af79daa41b
Sandeep Evani <evani.sandeep@ganitinc.com>
2025-11-01 17:10:33 +0530

    Add Comments and annotations

--- src/dao/core/dao.py
+++ src/dao/core/dao.py
@@ -9,9 +9,17 @@ from dao.utils.singleton import singleton
 @singleton
 class DataAccessObject:
     def __init__(self, data_stores: dict):
-        """ """
+        """Data Access Object is a convenience class, acting as an interface to access data,
+        data stores and perform many other data operations, All this heavy lifting is
+        abstracted from the user using the package and gives an easy and clean interface while writing
+        the main application code
+        """
 
+        # Register the data stores to the registry for common access
+        self.data_stores = data_stores
         DataStoreRegistry.initialize(data_stores)
 
+    # Create data accessor methods to access the respective data methods
     read = DataAccessor()
     write = DataAccessor()
+    run = DataAccessor()
