aa4b1d0689366ec25cc66aefd0a87743336c6674
Sandeep Evani <sandeepevani01@gmail.com>
2026-01-05 13:39:55 +0530

    Add S3 data objects

--- src/dao/data_object/__init__.py
+++ src/dao/data_object/__init__.py
@@ -1,4 +1,14 @@
 from .data_object import DataObject
 from .table_object import TableObject
+from .s3_data_objects import *
 
-__all__ = ["TableObject", "DataObject"]
+__all__ = [
+    "TableObject",
+    "DataObject",
+    "S3HudiObject",
+    "S3DeltaObject",
+    "S3IcebergObject",
+    "S3DirectoryObject",
+    "S3FileObject",
+    "S3TableObject",
+]
