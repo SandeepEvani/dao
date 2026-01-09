9f6454dcebce39601f845911b0163107edf2b7c3
Sandeep Evani <evani.sandeep@ganitinc.com>
2026-01-10 01:57:12 +0530

    Dissolve the DataStoreRegistry

--- src/dao/data_store/__init__.py
+++ src/dao/data_store/__init__.py
@@ -1,5 +1,4 @@
 from .data_store import DataStore
 from .data_store_factory import DataStoreFactory
-from .registry import DataStoreRegistry
 
-__all__ = ["DataStore", "DataStoreFactory", "DataStoreRegistry"]
+__all__ = ["DataStore", "DataStoreFactory"]
