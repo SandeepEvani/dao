17aad1c53c2180ef58e24cc6a499c05e8a5f35bb
Sandeep Evani <evani.sandeep@ganitinc.com>
2026-02-15 18:54:58 +0530

    chore: Remove unused S3 object imports from __init__.py

--- src/dao/data_object/__init__.py
+++ src/dao/data_object/__init__.py
@@ -1,21 +1,7 @@
 from .data_object import DataObject
-from .s3 import (
-    S3DeltaObject,
-    S3DirectoryObject,
-    S3FileObject,
-    S3HudiObject,
-    S3IcebergObject,
-    S3TableObject,
-)
 from .table_object import TableObject
 
 __all__ = [
     "TableObject",
     "DataObject",
-    "S3HudiObject",
-    "S3DeltaObject",
-    "S3IcebergObject",
-    "S3DirectoryObject",
-    "S3FileObject",
-    "S3TableObject",
 ]
